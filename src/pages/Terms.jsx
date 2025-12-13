830dbbf0992a44f674ae8fc5ae4601c34f349ba0
TPI-h <tpibabuhemalatha@gmail.com>
2025-12-13 06:18:37 +0530

    Create Terms.jsx

---
+++ src/pages/Terms.jsx
@@ -0,0 +1,18 @@
+const Terms = () => {
+  return (
+    <div className="max-w-4xl mx-auto px-6 py-10">
+      <h1 className="text-3xl font-bold mb-6">Terms & Conditions</h1>
+
+      <ul className="list-disc ml-6 space-y-2">
+        <li>Unmarried couples are not allowed</li>
+        <li>Guests below 18 years are not allowed</li>
+        <li>Valid Govt ID is mandatory</li>
+        <li>Pets are not allowed</li>
+        <li>Smoking is not allowed inside the premises</li>
+        <li>Outside food is not allowed</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Terms;
